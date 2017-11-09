721686a92184dd4c0f7e16ee19859d8c62aa4e47
LM239 <larsmartin239@gmail.com>
2017-11-09 22:34:34 +0100

    rettskriving

--- script/about.js
+++ script/about.js
@@ -13,8 +13,8 @@ document.getElementById('VexologyButton').addEventListener('click', function () 
 document.getElementById('LogoButton').addEventListener('click', function () {showBox(4);});
 
 //beskrivelser
-let aboutText = ["NTNUs Veksillologiske Interesse Gruppe, også kjent som NVIG, er en studentorganisasjon opptatt" +
-"        av spredningen av veksillologiens goder, spesifikt blant våre medstudenter ved NTNU." +
+let aboutText = ["NTNUs Vexillologiske Interesse Gruppe, også kjent som NVIG, er en studentorganisasjon opptatt" +
+"        av spredningen av vexillologiens goder, spesifikt blant våre medstudenter ved NTNU." +
 "        Inspirert av TED-talken til Roman Mars (som kan ses på NVIGs framside) om manglende engasjement anngående byflagg, ble NVIG grunnlagt" +
 "        av fem informatikkstudenter i 2017.", "Vi i NVIG er bekymret over mangelen av kunnskap om flag i samfunnet, derfor er målet vårt" +
 "        å spre informasjon og engasjement rundt flag. NVIG jobber med denne spredningen i håp om en" +
@@ -31,12 +31,12 @@ let aboutText = ["NTNUs Veksillologiske Interesse Gruppe, også kjent som NVIG, 
 "        De fleste byflagg går glemt på grunn av sitt dårlige design." +
 "        De fleste vexillogister mener at alle har rett til et bra flagg å samle seg bak," +
 "        og ved å engasjere deg i denne saken, kan DU utgjøre en forskjell!", "Hjernen symboliserer NVIGs medlemmers sinn, " +
-"flaggstangen er presentert som en plante som er spirt opp av hjernen, alt som skal til er" +
-" å plante veksillologiens frø i sinnet. Gjennom NVIg håper vi å oppnå akkuratt dette for andre medstudenter" +
-"på NTNU, og alle andre som kunne la seg friste av å lære noe om veksillologi"];
+"flaggstangen er presentert som en plante som spirer opp av hjernen, alt som skal til er" +
+" å plante vexillologiens frø i sinnet. Gjennom NVIg håper vi å oppnå akkuratt dette for andre medstudenter" +
+"på NTNU, og alle andre som kunne la seg friste av å lære noe om vexillologi"];
 
 //Overskrifter til beskrivelsene
-let aboutHeading = ["NVIG", "NVIG sitt mål", "MALE³", "Hvorfor Veksillologi?", "Logo"];
+let aboutHeading = ["NVIG", "NVIG sitt mål", "MALE³", "Hvorfor Vexillologi?", "Logo"];
 
 let about = document.getElementById("aboutDiv");
 let text  = document.createElement("div");
