4895a303d64587c1dd85fd39ceef8c8dd4628e51
LM239 <lego1234>
2017-11-09 11:09:57 +0100

    PG13 rating obtained

--- script/dataMaler/flagghistorie.js
+++ script/dataMaler/flagghistorie.js
@@ -7,7 +7,7 @@ Mening: mal for flagghistorie artikkelen, gir info til artikkelConstructor.js.
 var mainPhoto = {source:"bilder/fnBilde.png", alter:"artikkelbilde", heading:"Flaggdrama hos FN"};
 var details = {dato:"23/09/98",forfatter:"Torfrid Heigirsdottir"};
 var artikkelContent = [
-    {type:"p",content:"<strong>Historiker: </strong>Veksillologi er bedre enne sex!"},
+    {type:"p",content:"<strong>Historiker: </strong>Veksillologi er bedre enne ***!"},
     {type:"p",content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore" +
     "            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo" +
     "            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
