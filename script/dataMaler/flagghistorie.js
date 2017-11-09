136e11063b43d794ade40a3bc1c44865def61d8a
LM239 <larsmartin239@gmail.com>
2017-11-09 18:06:20 +0100

    fikset ff bug

--- script/dataMaler/flagghistorie.js
+++ script/dataMaler/flagghistorie.js
@@ -4,10 +4,10 @@ Skrevet av: Lars Martin Hodne
 Når: November 2017
 Mening: mal for flagghistorie artikkelen, gir info til artikkelConstructor.js.
 */
-var mainPhoto = {source:"bilder/fnBilde.png", alter:"artikkelbilde", heading:"Historien om flagg"};
+var mainPhoto = {source:"bilder/fnBilde.png", alter:"artikkelbilde", heading:"Artikkel om flaggdesign kommer"};
 var details = {dato:"23/09/98",forfatter:"Torfrid Heigirsdottir"};
 var artikkelContent = [
-    {type:"p",content:"<strong>Historiker: </strong>Veksillologi er bedre enne ***!"},
+    {type:"p",content:"Venter artikkel om flaggdesign"},
     {type:"p",content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore" +
     "            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo" +
     "            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
