8ac15b4ddef31326098d118c146b6c766e7c528d
LM239 <larsmartin239@gmail.com>
2017-11-09 19:03:06 +0100

    ny artikkel

--- script/dataMaler/veksillologi.js
+++ script/dataMaler/veksillologi.js
@@ -7,12 +7,20 @@ Mening: mal for flagghistorie artikkelen, gir info til artikkelConstructor.js.
 var mainPhoto = {source:"bilder/veksillologi.png", alter:"artikkelbilde", heading:"Vexillologi"};
 var details = {dato:"09/11/17",forfatter:"Lars Martin Hodne"};
 var artikkelContent = [
-    {type:"p",content:"<strong>Språket reflekterer ikke veksillologiens innflytelse i vårt lands historie</strong>"},
     {type:"p",content:"NVIGs mål har helt siden sin unfangelse vært å spre glede over, og kunnskap om veksillologi. " +
     "Flagg representerer både hvem vi er i det vi gjør, og det vi var i tiden før vår tid. Flagg står som kanskje" +
     "det mest meningsfylte symbolet noen eller noe kan representeres ved, om det er nasjonalflagget på brystet til" +
     "en idrettsutøver, eller på armen til en soldat omringet av krigens onder."},
     {type:"blockquote",content:'"Flagg representerer både hvem vi er i det vi gjør, og det vi var i tiden før vår tid."'},
-    {type:"p",content:"Med flaggenes innflytelse i vår hverdag er det NVIG syn at det norske språk må la dette faktum" +
-    "komme til uttrykk i sine ordbøker og definisjoner. Slik det er idag er det ingen norsk oversettelse for ""Ve"}
+    {type:"p",content:"Med flaggenes åpenbare innflytelse i vår hverdag er det NVIG syn at Vexillologien bør spres til alle og enhver." +
+    "Med seg fører vexillologien et hav av goder, ja, jeg vil si at etter man lærer å sette pris på flagg ser man verden på en annen måte enn før." +
+    "Hvorfor? Jo, fordi når man forstår hva som utgjør et godt eller dårlig flagg, kan man forstå designet til nesten hva som helst!" +
+    "Designreglene for flagg er enkle og kortfattede og kan, i realiteten anvendes som retningslninjer uansett hva man ønsker å lage."},
+    {type:"blockquote",content:'"Med seg fører vexillologien et hav av goder, ja, jeg vil si at etter man lærer å' +
+    ' sette pris på flagg ser man verden på en annen måte enn før."'},
+    {type:"p",content:"Med flaggenes åpenbare innflytelse i vår hverdag er det NVIG syn at Vexillologien bør spres til alle og enhver." +
+    "Med seg fører vexillologien et hav av goder, ja, jeg vil si at etter man lærer å sette pris på flagg ser man verden på en annen måte enn før." +
+    "Hvorfor? Jo, fordi når man forstår hva som utgjør et godt eller dårlig flagg, kan man forstå designet til nesten hva som helst!" +
+    "Designreglene for flagg er enkle og kortfattede og kan, i realiteten anvendes som retningslninjer uansett hva man ønsker å lage."},
+
 ];
