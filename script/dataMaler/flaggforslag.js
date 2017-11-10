d61549b063ec80584793d49b70c84837d126b506
LM239 <larsmartin239@gmail.com>
2017-11-10 19:29:27 +0100

    utvidet under mening:

--- script/dataMaler/flaggforslag.js
+++ script/dataMaler/flaggforslag.js
@@ -2,7 +2,8 @@
 Filnavn: flaggforslag.js
 Skrevet av: Emil Strand
 Når: November 2017
-Mening: mal for flaggforslag artikkelen, gir info til artikkelConstructor.js. oppfylle krav om modularitet
+Mening: gir info til artikkelConstructor.js. oppfylle krav om modularitet
+gjør at man kan bytte overskrifter på forsiden, artikkelen og i headeren, bare ved å bytte variabelen en plass
 */
 
 //Legger til hovedbildet i artikkelen
--- script/dataMaler/ntnui.js
+++ script/dataMaler/ntnui.js
@@ -2,7 +2,8 @@
 Filnavn: ntnui.js
 Skrevet av:Lars Martin Hodne
 Når: November 2017
-Mening: mal for ntnui artikkelen, gir info til artikkelConstructor.js. oppfylle krav om modularitet
+Mening: gir info til artikkelConstructor.js. oppfylle krav om modularitet
+gjør at man kan bytte overskrifter på forsiden, artikkelen og i headeren, bare ved å bytte variabelen en plass
 */
 
 //tabelldata brukt på siden
