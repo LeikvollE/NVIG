5339d8a2c986b6c32ae3c2cbc3d0d4199df4cb86
LM239 <lego1234>
2017-11-10 12:48:51 +0100

    forbedret constructor

--- script/dataMaler/flaggforslag.js
+++ script/dataMaler/flaggforslag.js
@@ -50,7 +50,7 @@ var artikkelContent = [
     "så tykt grønt som gult eller svart er valgt for at proporsjonene skal være lette å anvende for å lage flagg ut av " +
     "fargede gjenstander i samme størrelse: Du kan strø en kake med Non Stop à 2+2 rader grønne, 1+1 rad gule og 1 rad " +
     "svarte eller publisere et innlegg på sosiale med 2+2 grønne, 1+1 gule og 1 svart hjerte.\n"},
-    {type: "img", source:"bilder/fbpost.png", alter: "Facebook post", id: "FBpost"},
+    {type: "img", source:"bilder/fbpost.png", alter: "Facebook post", id: "fbBilde"},
     {type: "p", content:"Flaggproporsjonene 7:10 er valgt fordi flagg pleier å være rektangulære og at det skal " +
     "være lett å regne på det om man skal lage det for hånd. Om flagget blir litt lenger eller litt kortere er ganske " +
     "uvesentlig, men huskeregelen om å gange stripelengde med fem for gult og svart eller ti for grønt er et bra " +
