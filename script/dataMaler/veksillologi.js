0a21def8ae12a3c0ba565ae62cf8b3dfc44cabfa
LM239 <larsmartin239@gmail.com>
2017-11-10 19:05:24 +0100

    fjernet overflødig css

--- script/dataMaler/veksillologi.js
+++ script/dataMaler/veksillologi.js
@@ -2,6 +2,7 @@
 Filnavn: veksillologi.js
 Skrevet av: Lars Martin Hodne
 Når: November 2017
-Mening: mal for vexillologi artikkelen, gir info til artikkelConstructor.js. oppfylle krav om modularitet
+Mening: gir info til artikkelConstructor.js. oppfylle krav om modularitet
+gjør at man kan bytte overskrifter på forsiden, artikkelen og i headeren, bare ved å bytte variabelen en plass
 */
 var mainPhoto = {source:artikler[1][1].bilde, alter:"artikkelbilde", heading:artikler[1][1].tittel};
