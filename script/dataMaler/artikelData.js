609dd4fed82e4161b95a1246b3ba528e3b0d8977
LM239 <lego1234>
2017-11-11 12:35:52 +0100

    kommentering

--- script/dataMaler/artikelData.js
+++ script/dataMaler/artikelData.js
@@ -1,10 +1,18 @@
 //Mening: Legger til artiklene på framsiden og legger inn bildet over den korresponderende artikkelen
+/*
+Filnavn: artikelData.js
+Skrevet av: Lars Martin Hodne, Einar Viddal
+Når: November 2017
+Mening: Legger til artiklene på framsiden og legger inn bildet over den korresponderende artikkelen.
+gjør at man kan bytte bilde på forsiden og artiklene gjennom en fil,
+og bytte oversskrift i header, forside og artikkel i samme fil
+*/
 
-var artikler = [
+let artikler = [
     [
         {
-            tittel:"Til alle med hjerte for NTNUI", //Tittel fikser tittelen til artikkelen på framsiden
-            bilde:"artikkelTopp.png",               //Legger bildet til artikkelen
+            tittel:"Til alle med hjerte for NTNUI", //fikser tittelen til artikkelen på framsiden, i artikkelen, og i header
+            bilde:"artikkelTopp.png",               //Legger bildet til artikkelen, og på framsiden
             fil:"til-alle-med-hjerte-i-ntnui.html"  //HTML-filen artikkelen skal linke til
         }
     ],
@@ -39,7 +47,7 @@ var artikler = [
     ]
 ];
 
-var ekstraSider = [
+let ekstraSider = [ //sider som ikke vises på forsiden som artikkel ligger i ekstrasider
     {
         tittel:"Om oss",
         fil:"about.html"
