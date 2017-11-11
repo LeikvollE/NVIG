81437eaf5b5ca61102f2142d6b28c0982f0a49e4
emilstra <emil.strand@lyse.net>
2017-11-11 12:05:43 +0100

    Kommentert js filer

--- script/dataMaler/data.js
+++ script/dataMaler/data.js
@@ -2,10 +2,10 @@
 Filnavn: data.js
 Skrevet av: Lars Martin Hodne
 Når: November 2017
-Mening: tilby filplasseringer og navn når BildeGalleriJs spør etter dette, .
+Mening: tilby filplasseringer og navn når BildeGalleriJs spør etter dette.
 */
 
-let bilder = [
+let bilder = [ //Linker til bildene i bildegalleriet.
     "Flagg/Australia.png","Flagg/Norge.png", "Flagg/Brasil.png","Flagg/Canada.png",
     "Flagg/Danmark.png","Flagg/Estland.png","Flagg/Etiopia.png", "Flagg/Finland.png",
     "Flagg/Frankrike.png","Flagg/Hellas.png","Flagg/Indonesia.png","Flagg/Vietnam.png",
@@ -14,7 +14,7 @@ let bilder = [
     "Flagg/S_korea.png","Flagg/Nepal.png","Flagg/Qatar.png","Flagg/Polen.png",
     "Flagg/Russland.png","Flagg/New_Zealand.png","Flagg/palestina.png"
 ];
-let land = [
+let land = [ //Navnet på de ulike landene representert i galleriet.
     "Australia","Norge", "Brasil","Canada",
     "Danmark","Estland","Etiopia", "Finland",
     "Frankrike","Hellas","Indonesia","Vietnam",
@@ -24,7 +24,7 @@ let land = [
     "Russland","New Zealand","Palestina"
 ];
 
-let beskrivelser = [
+let beskrivelser = [ //Linker til txt-filene som inneholder beskrivelsene til de ulike flaggene.
     "beskrivelser/Australia.txt","beskrivelser/Norge.txt", "beskrivelser/Brasil.txt","beskrivelser/Canada.txt",
     "beskrivelser/Danmark.txt", "beskrivelser/Estland.txt","beskrivelser/Etiopia.txt", "beskrivelser/Finland.txt",
     "beskrivelser/Frankrike.txt","beskrivelser/Hellas.txt","beskrivelser/Indonesia.txt","beskrivelser/Vietnam.txt",
--- script/dataMaler/quizer.js
+++ script/dataMaler/quizer.js
@@ -7,50 +7,50 @@ Mening: Beskrive innholdet i quizene på quiz siden, dvs spørsmålene, svaralte
 
 let quizer = [
   {
-    "navn":"Hvor godt kjenner du disse flaggene?",
-    "sporsmaal":[
+    navn:"Hvor godt kjenner du disse flaggene?",
+    sporsmaal:[
       "Hvilket stjernebilde representerer de 5 stjernene i den høyre delen av Australias flagg?",
       "Hva representerer stripene i Hellas flagg?",
       "Nepals flagg er unikt fordi..",
       "Hva er opprinnelsen til korset i Sveits flagg?",
       "Qatars flagg er unikt fordi.."
     ],
-    "svar":[
+    svar:[
       ["Sørkorset","Delfinen", "Alteret","Paradisfuglen"],
       ["De 5 viktige importvarene i Hellas: mat, petroleum, stål, olivenolje og tobakk", "De 9 største byene i Hellas.", "De 5 fem samfunnsklassene i Athen på den tiden Solon oppfant demokratiet.", 'De 9 stavelsene i en gresk læresetning "frihet eller døden".'],
       ["Det er det eneste nasjonalflagget som ikke er firkantet", "Det er har mer enn ett symbol i seg", "Det tar i bruk det gylne snitt", "Det tar i bruk det omvendte gylne snitt"],
       ["Det kristne korset", "Et kampflagg for sveitsiske forsvarstyrker", "Det røde korset, et av Røde Kors sine symboler", "Ordenen Riddere av det hvite kors"],
       ["Der det eneste flagget med 2 farger", "Det har det høyeste forholdet mellom sidene, av alle nasjonalflagg", "Det skal henges opp med begge endene festet til flaggstangen", "Man fester undersiden til flaggstangen"]
     ],
-    "bilder":[
+    bilder:[
       "bilder/Flagg/Australia.png",
       "bilder/Flagg/Hellas.png",
       "bilder/Flagg/Nepal.png",
       "bilder/Flagg/Sveits.png",
       "bilder/Flagg/Qatar.png"
     ],
-    "riktigeSvar":[0,3,0,1,1]
+    riktigeSvar:[0,3,0,1,1]
   },
   {
-    "navn":"Gjett på europeiske flagg",
-    "sporsmaal":[
+    navn:"Gjett på europeiske flagg",
+    sporsmaal:[
       "Hvilket land har dette flagget?",
       "Hvilket land har dette flagget?",
       "Hvilket land har dette flagget?",
       "Hvilket land har dette flagget?"
     ],
-    "svar":[
+    svar:[
       ["Norge", "Danmark", "Sveits", "Nederland"],
       ["Polen", "Estland", "Østerrike", "Ungarn"],
       ["Hellas", "Italia", "Albania", "Montenegro"],
       ["Bulgaria", "Romania", "Andorra", "Moldova"]
     ],
-    "bilder":[
+    bilder:[
       "bilder/Flagg/Danmark.png",
       "bilder/Flagg/Estland.png",
       "bilder/Flagg/Hellas.png",
       "bilder/Flagg/ad.png"
     ],
-    "riktigeSvar":[1,1,0,2]
+    riktigeSvar:[1,1,0,2]
   }
 ];
