a709c6833e0b5b74863b86a62417d96a2a02af60
Eirik <eirik.leikvoll@gmail.com>
2017-11-11 01:10:37 +0100

    Fikset collision error i spillet

--- game/javascript/Spill.js
+++ game/javascript/Spill.js
@@ -125,10 +125,10 @@ function move(elapsedSinceLastLoop) {
 
     checkCollisionsHorizontal(direction);
 
-    if (!(tileAtScreenPos(player.xPos+player.speed*direction, player.yPos).solid ||
-        tileAtScreenPos(player.xPos+player.speed*direction, player.yPos+20).solid ||
-        tileAtScreenPos(player.xPos+20+player.speed*direction, player.yPos).solid ||
-        tileAtScreenPos(player.xPos+20+player.speed*direction, player.yPos+20).solid)) {
+    if (!(tileAtScreenPos(player.xPos+player.speed*direction*elapsedSinceLastLoop*75, player.yPos).solid ||
+        tileAtScreenPos(player.xPos+player.speed*direction*elapsedSinceLastLoop*75, player.yPos+20).solid ||
+        tileAtScreenPos(player.xPos+20+player.speed*direction*elapsedSinceLastLoop*75, player.yPos).solid ||
+        tileAtScreenPos(player.xPos+20+player.speed*direction*elapsedSinceLastLoop*75, player.yPos+20).solid)) {
         player.xPos += player.speed*direction*elapsedSinceLastLoop*75;
     }
 
